e3dffbdd8fb344f01fe6cc1ac4b630ab37956d41
Khaled <khaled@rubix-studio.com>
2022-02-19 12:35:01 +0200

    image is detected then video plays but it doesn't pause when the image is not seen

--- App.js
+++ App.js
@@ -8,28 +8,34 @@ import {
   ViroARSceneNavigator,
   ViroMaterials,
   ViroAnimations,
+  ViroARTrackingTargets,
+  ViroARImageMarker,
+  ViroVideo,
 } from '@viro-community/react-viro';
 
 const InitialScene = () => {
+  const [muted, setMuted] = useState(false);
   ViroMaterials.createMaterials({
     wood: {
       diffuseTexture: require('./assets/texture.jpeg'),
     },
   });
-  ViroAnimations.registerAnimations({
-    rotate: {
-      duration: 2500,
-      properties: {rotateY: '+=90'},
+  // ViroAnimations.registerAnimations({
+  //   rotate: {
+  //     duration: 2500,
+  //     properties: {rotateY: '+=90'},
+  //   },
+  // });
+  ViroARTrackingTargets.createTargets({
+    targetOne: {
+      source: require('./assets/chocolate.jpeg'),
+      orientation: 'Up',
+      physicalWidth: 0.08, // real world width in meters
     },
   });
   return (
     <ViroARScene>
-      {/* <ViroText
-        text="Hello World"
-        position={[0, 2, -5]}
-        styles={{fontSize: '30'}}
-      /> */}
-      <ViroBox
+      {/* <ViroBox
         height={2}
         length={2}
         width={2}
@@ -37,7 +43,36 @@ const InitialScene = () => {
         scale={[0.2, 0.2, 0.2]}
         materials={['wood']}
         animation={{name: 'rotate', loop: true, run: true}}
-      />
+      /> */}
+      <ViroARImageMarker
+        target={'targetOne'}
+        onAnchorFound={() => {
+          console.log('found');
+          setMuted(false);
+        }}
+        onAnchorRemoved={() => {
+          console.log('removed');
+          setMuted(true);
+        }}>
+        {/* <ViroBox
+          height={2}
+          length={2}
+          width={2}
+          position={[0, 0, 0]}
+          scale={[0.2, 0.2, 0.2]}
+          materials={['wood']}
+          animation={{name: 'rotate', loop: true, run: true}}
+        /> */}
+        <ViroVideo
+          source={require('./assets/believer.mp4')}
+          height={0.05}
+          width={0.08}
+          loop={true}
+          position={[0, 0, 0]}
+          muted={muted}
+          transformBehaviors={['billboard']}
+        />
+      </ViroARImageMarker>
     </ViroARScene>
   );
 };
